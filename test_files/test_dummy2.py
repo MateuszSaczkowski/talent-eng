a1f76d30e7278188e9bb9a72366e15676e905471
MateuszSaczkowski <mateusz.saczkowski@globallogic.com>
2022-11-06 13:07:03 +0100

    new stuff

---
+++ test_files/test_dummy2.py
@@ -0,0 +1,18 @@
+def test_dummyson():
+    assert True is True
+    
+
+def testbuddy():
+    assert 1 == 1
+
+def testsmth():
+    assert 'a' == 'd'
+
+def test_smth():
+    a = 3
+    b = 2
+    assert a == b
+
+def test_th():
+    awfsesr
+    assert 5 == 3
